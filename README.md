b52eb48da9fd5030dc21b9b501f8057e30a889c2
Benjamin Schmidt <bmschmidt@gmail.com>
2014-06-18 20:10:21 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,120 @@
+D3 Trail layout
+================
+
+
+This is a layout function for creating paths in D3 where (unlike the native d3.svg.line() element) you need to apply specific geometries to each element of the line.
+
+This makes it possible to do things like staggered entry ([for instance, in a random walk](http://benschmidt.org/D3trail/pathdemo.html)); or lines of variable width ([for instance, in this reproduction of the line part of Minard's famous Napoleon map](http://benschmidt.org/minard.html).
+
+
+
+You instantiate it by calling the function: once parameters are set, run the `layout()` method to get values back.
+
+
+A bare bones example might be:
+
+``` {js}
+trail = d3.layout.trail().coordType('xy')
+
+new = trail.data(whatever).layout()
+
+paths = svg.selectAll("line").data(new)
+
+paths.enter()
+.append('line')
+.attr("x1",function(d) {return d.x1}) 
+.attr("y1",function(d) {return d.y1}) 
+.attr("y2",function(d) {return d.y2}) 
+.attr("x2",function(d) {return d.x2}) 
+
+```
+
+Features include:
+
+1. Controls to define multiple groups of simultaneous lines by setting `grouping`.
+2. Options to return data either with `x1`,`x2`,`y1`, and `y2` elements to use with svg lines *or* with a `coordinates` object to use with geoprojections by setting `coordType`.
+3. Inbuilt methods to easily define `opacity` in the data as an evaporating trail in time like the frames in [my maps of shipping routes](http://sappingattention.blogspot.com/2012/04/visualizing-ocean-shipping.html).
+4. Control over sorting.
+5. Access to both points in the bound data, so that appearance of a segment can be defined by data about the destination point, the origin, or some interaction of them.
+
+
+API
+===
+
+Creation
+--------
+
+Create a trail object
+```
+trail = d3.layout.trail()
+```
+
+trail.data()
+-----------
+
+Set (or with no arguments, return) the data to be plotted. Each object in the return values will correspond to two consecutive points in this array.
+
+
+trail.positioner()
+-------------------
+
+Set (or with no arguments, return) a function used to specify the x and y values for each **point**.
+
+By default, it returns simply [x,y] for the data:
+
+```
+   positioner = function(datum) {        return [datum.x,datum.y]    }
+```
+
+You can also drop in any map projection as the positioner.
+
+
+trail.coordType()
+-----------------
+
+Set (or with no arguments, return) the format of positions for the return values. Can be either
+1. `"xy"`, in which case you'll get `x1`,`x2`,`y1`, and `y2` elements suitable for use with svg lines, or
+2. `"coordinates"`, in which case the objection will get a `coordinates` element consisting of an array of points so you can simply create a d3.geo.path() element. (Useful if you want to preserve great circle arcs for your paths, for instance; in this case the positioner should return lat and lon, not the projected values, so you don't project them twice.)
+
+
+trail.grouping()
+----------------
+
+Set (or with no arguments, return) the grouping 
+
+
+
+trail.sort()
+-------------
+
+Set (or with no arguments, return) the function used to order the points.
+
+If not set, points will be ordered in the direction they went in.
+
+
+
+
+
+
+
+And then set the parameters
+
+
+
+
+Time-specific elements
+-----------------------
+
+### trail.time()
+
+Set (or with no arguments, return) a function that returns how to access the time data. (Must be numeric: dates not yet supported).
+
+### trail.currentTime()
+
+Set (or with no arguments, return) the current time (ie, that for which opacity should be 1); all times after this will be dropped from the returned set.
+
+### trail.decayRange()
+
+Set (or with no arguments, return) the number of time units to be displayed: The opacity of segments ending at `trail.currentTime() - trail.decayRange()` will be zero, and all earlier segments will not be displayed
+
+
---
+++ pathlayout.js
@@ -0,0 +1,149 @@
+d3.layout.trail = function() {
+
+    var that = {}; //output object
+
+    var time = function() {}, //how to access the time data (must be numeric--should but doesn't handle dates);
+        currentTime, //points of this time will be display with full opacity;
+        //later points are dropped;
+        decayRange,  //points of this age will have opacity 0. If either currentTime or decayRange is not defined, opacity will be added as some undefined values.
+        data,       // the data being arranged
+        positioner, // a function that returns the [x,y] for the point.
+        sort,       // a function specifying the sort order
+        coordType = 'coordinates', //either "coordinates" or "xy"; if the first, returns a "coordinates" array; if the latter, returns x1,y1,x2,y2
+        grouping; // a function to split the data up into multiple segments;
+
+    grouping = function(d) {
+        return 1
+    }
+
+    positioner = function(datum) {
+        //given a datum, returns an [x,y] array.
+        //Might be a projection, for example, or a scale output.
+        return [datum.x,datum.y]
+    }
+
+
+    lineToSegments = function(values) {
+        //the returned array will be filtered to only include segments that fit the defined values.
+
+        if (currentTime != undefined & decayRange != undefined) {
+            values = values.filter(function(d) {
+                return (time(d) <= currentTime && time(d) >= (currentTime-decayRange))
+            })
+        }
+
+
+
+        values = d3
+            .nest()
+            .key(function(d) {return grouping(d)})
+            .entries(values);
+
+        tmp = values;
+
+        output = [];
+
+
+        var i = 0
+        values.forEach(function(element) {
+            i++;
+	    if (sort!=undefined) {
+		element.values.sort(sort)
+	    }
+            if (i==1) {
+                //console.log(element)
+	    }
+            var values = element.values;
+
+            for (var i = 0; i < (values.length); i++) {
+                var current = values[i];
+                if (values[i-1] != undefined) {
+                    current.previous = values[i-1]
+                    if (coordType=="coordinates") {
+                        current.coordinates = [
+                            positioner(values[i-1]),
+                            positioner(values[i])
+                        ]
+                    } else if (coordType=="xy") {
+                        var a = positioner(values[i-1]),
+                            b = positioner(values[i]);
+                        current.x1=a[0]
+                        current.y1=a[1]
+                        current.x2=b[0]
+                        current.y2=b[1]
+                    }
+                    current.type = "LineString";
+                    //opacity will probably be this: the percentage of the decay range ago that it was.
+                    //Early tests should guarantee a result between 0 and 1.
+                }
+                current.opacity = 1-(currentTime-time(current))/decayRange
+            }
+	    output = output.concat(values);
+        })
+        return output;
+    }
+
+    that.layout = function() {
+        output = lineToSegments(data);
+        return output;
+    }
+
+    that.coordType = function(x) {
+        if (!arguments.length) return coordType;
+        coordType= x
+        return that
+    }
+
+    that.grouping = function(x) {
+        if (!arguments.length) return grouping;
+        grouping= x
+        return that
+    }
+
+    that.time = function(x) {
+        if (!arguments.length) return time;
+        time = x
+        return that
+    }
+
+    that.currentTime = function(x) {
+        if (!arguments.length) return currentTime;
+        currentTime= x
+        return that
+    }
+
+    that.decayRange = function(x) {
+        if (!arguments.length) return decayRange;
+        decayRange= x
+        return that
+    }
+
+    that.data = function(x,append) {
+        if (!arguments.length) return data;
+
+        if (append) {
+            data = data || [];
+            data = data.concat(x)
+        } else {
+            data = x
+        }
+
+        return that
+    }
+
+    that.positioner = function(x) {
+        if (!arguments.length) return positioner;
+        positioner = x
+        return that
+    }
+    that.sort = function(x) { 
+ 	if (!arguments.length) return sort; 
+	    sort= x
+ 	return that
+    } 
+    
+
+
+    return that;
+
+}
